e6bf018c87cf1023a14ebd92e92d1fe1757f7fca
Vishalpateldodla <117738669+Vishalpateldodla@users.noreply.github.com>
2023-02-20 11:34:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 //your code here
+// Get all the grid items and store them in an array
+const gridItems = document.querySelectorAll('.grid-item');
 
 function reset() {
-    for (i = 1; i <= 9; i++) {
-        const block = document.getElementById(`${i}`);
-        block.style.backgroundColor = "transparent";
-    }
+  // Loop through each grid item and set its background color to transparent
+  for (let i = 0; i < gridItems.length; i++) {
+    gridItems[i].style.backgroundColor = 'transparent';
+  }
 }
-  
+
+// Add event listener to the "Reset" button
 document.getElementById('reset_button').addEventListener('click', reset);
 
-document.getElementById('change_button').addEventListener('click', () => {
-    reset();
-    const blockId = document.getElementById("block_id").value;
-    const color = document.getElementById("colour_id").value;
-    // alert(colorId)
-    const block = document.getElementById(`${blockId}`);
+// Add event listener to the "Change" button
+document.getElementById('change_button').addEventListener('click', function() {
+  // Get the block ID and color input from the user
+  const blockId = document.getElementById('block_id').value;
+  const color = document.getElementById('colour_id').value;
+
+  // Find the grid item with the corresponding ID and set its background color to the user input
+  const block = document.getElementById(blockId);
+  if (block) {
     block.style.backgroundColor = color;
+  } else {
+    alert('Invalid block ID!');
+  }
 });
